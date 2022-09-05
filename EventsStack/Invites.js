87557fcc159180469aac0cdfaea8e4e19fd4ec38
useless-creamy <94379739+useless-creamy@users.noreply.github.com>
2022-09-05 17:30:34 +0200

    Delete Invites.js

--- EventsStack/Invites.js
+++
@@ -1,6 +0,0 @@
-const { Client, EmbedBuilder } = require("discord.js");
-const client = require("../Structures/main");
-const invites = new Map();
-const LogDB = require("../Structures/Schema/LogsChannel");
-const wait = require("timers/promises").setTimeout
-const { Discord } = require("discord-id");
